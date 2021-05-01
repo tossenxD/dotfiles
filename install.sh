1f28e49a0d8807c44acfab0056966c21ad08c2b9
tossenxD <thorbjorn.bb@gmail.com>
2021-05-01 22:17:34 +0000

    update

--- install.sh
+++ install.sh
@@ -1,27 +1,27 @@
-printf "Change directory to ~/\n"
+printf "\nChange directory to ~/\n\n"
 cd ~/
-printf "Installing basic packages\n"
+printf "\nInstalling basic packages\n\n"
 # Install basic packages
 sudo pacman -S --noconfirm neovim bspwm sxhkd rxvt-unicode dmenu xorg xorg-xinit nitrogen firefox nautilus ranger screengrab zathura amd-ucode networkmanager network-manager-applet xf86-video-amdgpu man-db man-pages
 
 # Install yay
-printf "Make directory aur/\n"
-mkdir aur
+printf "\nMake directory aur/ downloads/ pictures/\n\n"
+mkdir aur downloads
 cd aur
-printf "Install AUR package-manager yay\n"
+printf "\nInstall AUR package-manager yay from aur/\n\n"
 git clone https://aur.archlinux.org/yay.git
 cd yay/
-makepkg -si
+makepkg -si --noconfirm
 cd ~/
 
 # Install polybar
-printf "Using yay to install polybar\n"
-yay -S polybar siji ttf-unifont --mflags "--skipchecksums --skippgpcheck"
+printf "\nUsing yay to install polybar\n\n"
+yay -S --noconfirm polybar siji ttf-unifont
 
 # Copy configurations
-printf "Change directory to ~/git/OS-configs\n"
+printf "\nChange directory to ~/git/OS-configs\n\n"
 cd ~/git/OS-configs
-printf "Copying all config files"
+printf "\nCopying all config files\n\n"
 cp -r .colors/ ~/
 cp -r .config/ ~/
 cp .gitconfig ~/
@@ -29,4 +29,13 @@ cp -r scripts/ ~/
 cp .xinitrc ~/
 cp .Xresources ~/
 cp .bashrc ~/
-printf "Installation is over!"
+cp wallpaper.png ~/pictures/
+
+# Set options
+printf "\nSetting wallpaper\n\n"
+nitrogen --set-auto --save ~/pictures/wallpaper.png
+printf "\nEnabling NetworkManager\n\n"
+sudo systemctl enable NetworkManager.service
+sudo systemctl start NetworkManager.service
+
+printf "\nInstallation is over!\n"
