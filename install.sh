473e9a5b4c55d3c5c1983366657eb04a81e36349
tossenxD <thorbjorn.bb@gmail.com>
2021-05-01 22:29:29 +0000

    update

--- install.sh
+++ install.sh
@@ -1,27 +1,27 @@
-printf "\nChange directory to ~/\n\n"
+printf "\n>Change directory to ~/\n\n"
 cd ~/
-printf "\nInstalling basic packages\n\n"
+printf "\n>Installing basic packages\n\n"
 # Install basic packages
-sudo pacman -S --noconfirm neovim bspwm sxhkd rxvt-unicode dmenu xorg xorg-xinit nitrogen firefox nautilus ranger screengrab zathura amd-ucode networkmanager network-manager-applet xf86-video-amdgpu man-db man-pages
+sudo pacman -S --noconfirm neovim bspwm sxhkd rxvt-unicode dmenu xorg xorg-xinit nitrogen firefox nautilus ranger screengrab zathura amd-ucode xf86-video-amdgpu man-db man-pages
 
 # Install yay
-printf "\nMake directory aur/ downloads/ pictures/\n\n"
-mkdir aur downloads
+printf "\n>Make directory aur/ downloads/ pictures/\n\n"
+mkdir aur pictures downloads
 cd aur
-printf "\nInstall AUR package-manager yay from aur/\n\n"
+printf "\n>Install AUR package-manager yay from aur/\n\n"
 git clone https://aur.archlinux.org/yay.git
 cd yay/
 makepkg -si --noconfirm
 cd ~/
 
 # Install polybar
-printf "\nUsing yay to install polybar\n\n"
+printf "\n>Using yay to install polybar\n\n"
 yay -S --noconfirm polybar siji ttf-unifont
 
 # Copy configurations
-printf "\nChange directory to ~/git/OS-configs\n\n"
+printf "\n>Change directory to ~/git/OS-configs\n\n"
 cd ~/git/OS-configs
-printf "\nCopying all config files\n\n"
+printf "\n>Copying all config files\n\n"
 cp -r .colors/ ~/
 cp -r .config/ ~/
 cp .gitconfig ~/
@@ -32,10 +32,7 @@ cp .bashrc ~/
 cp wallpaper.png ~/pictures/
 
 # Set options
-printf "\nSetting wallpaper\n\n"
+printf "\n>Setting wallpaper\n\n"
 nitrogen --set-auto --save ~/pictures/wallpaper.png
-printf "\nEnabling NetworkManager\n\n"
-sudo systemctl enable NetworkManager.service
-sudo systemctl start NetworkManager.service
 
-printf "\nInstallation is over!\n"
+printf "\n>Installation is over!\n"
