133a5030ef9511d7af1472153a3ddf8b7a7f59db
tossenxD <thorbjorn.bb@gmail.com>
2021-05-19 21:46:14 +0200

    neofetch

--- install_extra.sh
+++ install_extra.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 printf "\n>Installing extra packages\n\n"
 # Install extra packages
-sudo pacman -Syu --noconfirm texlive-most dotnet-runtime jdk-openjdk
+sudo pacman -Syu --noconfirm texlive-most dotnet-runtime jdk-openjdk neofetch
 
