b060843771af5d4644b1395ac74eb8e5ed7d60e9
tossenxD <thorbjorn.bb@gmail.com>
2023-09-05 19:13:26 +0200

    added a file-manager to the toolbar of icewm

--- .basic.sh
+++ .basic.sh
@@ -3,7 +3,7 @@ echo ">Beginning basic installation"
 
 echo ">Installing pacman packages"
 # Package install
-sudo pacman -Syu --noconfirm neovim bspwm sxhkd rxvt-unicode dmenu xorg xorg-xinit nitrogen firefox nautilus ranger screengrab zathura zathura-pdf-poppler amd-ucode xf86-video-amdgpu man-db man-pages python python-pip xsel mlocate alsa-utils alsa-firmware pulseaudio pulseaudio-alsa pulseaudio-jack pulsemixer trash-cli ueberzug w3m sxiv python-pygments swappy grim slurp wl-clipboard otf-font-awesome sway swaybg wofi alacritty waybar xorg-xwayland wayland xdotool icewm
+sudo pacman -Syu --noconfirm neovim bspwm sxhkd rxvt-unicode dmenu xorg xorg-xinit nitrogen firefox nautilus ranger screengrab zathura zathura-pdf-poppler amd-ucode xf86-video-amdgpu man-db man-pages python python-pip xsel mlocate alsa-utils alsa-firmware pulseaudio pulseaudio-alsa pulseaudio-jack pulsemixer trash-cli ueberzug w3m sxiv python-pygments swappy grim slurp wl-clipboard otf-font-awesome sway swaybg wofi alacritty waybar xorg-xwayland wayland xdotool icewm pcmanfm
 pip install neovim neovim-remote
 
 # Home setup
