18bd522bd84e23324bc5a5e30c4a8b367c12aaaa
tossenxD <thorbjorn.bb@gmail.com>
2024-04-14 19:54:13 +0200

    dark theme, sway workstyle waybar format, floating terminals and more

--- .bash_profile
+++ .bash_profile
@@ -6,7 +6,12 @@
 export GPG_TTY=$(tty)
 
 if [ "$(tty)" = "/dev/tty1" ]; then
-  exec sway
+  export __GLX_VENDOR_LIBRARY_NAME=amdgpu
+  export DRI_PRIME=0
+  export GTK_THEME=Adwaita:dark
+  export GTK2_RC_FILES=/usr/share/themes/Adwaita-dark/gtk-2.0/gtkrc
+  export QT_STYLE_OVERRIDE=Adwaita-Dark
+  exec sway --unsupported-gpu
 fi
 
 if [ "$(tty)" = "/dev/tty2" ]; then
--- .basic.sh
+++ .basic.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo ">Beginning basic installation"
 
-echo ">Installing pacman packages"
+echo ">Installing packages"
 # Package install
-sudo pacman -Syu --noconfirm neovim bspwm sxhkd rxvt-unicode dmenu xorg xorg-xinit nitrogen firefox nautilus ranger screengrab zathura zathura-pdf-poppler amd-ucode xf86-video-amdgpu man-db man-pages python python-pip xsel mlocate alsa-utils alsa-firmware pulseaudio pulseaudio-alsa pulseaudio-jack pulsemixer trash-cli ueberzug w3m sxiv python-pygments swappy grim slurp wl-clipboard otf-font-awesome sway swaybg wofi alacritty waybar xorg-xwayland wayland xdotool icewm pcmanfm rofi emacs cmark
+sudo pacman -Syu --noconfirm neovim bspwm sxhkd rxvt-unicode dmenu xorg xorg-xinit nitrogen firefox nautilus ranger screengrab zathura zathura-pdf-poppler amd-ucode xf86-video-amdgpu man-db man-pages python python-pip xsel mlocate alsa-utils alsa-firmware pulseaudio pulseaudio-alsa pulseaudio-jack pulsemixer trash-cli ueberzug w3m sxiv python-pygments swappy grim slurp wl-clipboard otf-font-awesome sway swaybg wofi alacritty waybar xorg-xwayland wayland xdotool icewm pcmanfm rofi emacs cmark gnome-themes-extra
 pip install neovim neovim-remote
 
 # Home setup
@@ -17,8 +17,8 @@ git clone https://aur.archlinux.org/yay.git
 cd $HOME/aur/yay/
 makepkg -si --noconfirm
 
-# polybar install
-echo ">Installing polybar"
-yay -S --noconfirm siji ttf-unifont polybar
+# AUR package install
+echo ">Installing AUR packages"
+yay -S --noconfirm siji ttf-unifont polybar workstyle-git adwaita-qt5-git adwaita-qt6-git
 
 echo ">Finished basic installation"
