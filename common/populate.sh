77cf84dfc380fa8298fb830f64308866ebad9315
tossenxD <thorbjorn.bb@proton.me>
2024-10-05 11:35:20 +0200

    more robust common dotfiles

--- common/populate.sh
+++ common/populate.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 PDIR=$(dirname $(realpath $0))
-mkdir -p $HOME/.config $HOME/.emacs.d
+mkdir -p $HOME/.config
 
 ln -sf $PDIR/.config/sway $HOME/.config
 ln -sf $PDIR/.config/swappy $HOME/.config
@@ -9,17 +9,12 @@ ln -sf $PDIR/.config/alacritty $HOME/.config
 ln -sf $PDIR/.config/wofi $HOME/.config
 ln -sf $PDIR/.config/workstyle $HOME/.config
 
-ln -sf $PDIR/.config/nvim/init.vim $HOME/.config/nvim
-ln -sf $PDIR/.config/nvim/plugconfigs $HOME/.config/nvim
-ln -sf $PDIR/.config/nvim/syntax $HOME/.config/nvim
-
+ln -sf $PDIR/.emacs.d $HOME
+ln -sf $PDIR/.config/nvim $HOME/.config
 ln -sf $PDIR/.config/ranger $HOME/.config
 ln -sf $PDIR/.config/zathura $HOME/.config
 
 ln -sf $PDIR/.bashrc $HOME
 ln -sf $PDIR/.bash_profile $HOME
 
-ln -sf $PDIR/.emacs.d/init.el $HOME/.emacs.d
-ln -sf $PDIR/.emacs.d/emacs.org $HOME/.emacs.d
-
-ln -sf $PDIR/.scripts $HOME
+ln -sf $PDIR/scripts $HOME
